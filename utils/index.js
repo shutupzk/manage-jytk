e1525d66a3699eb8405ddf531c90d93962eae4ee
wangfang <wangfang_zry_0824@163.com>
2017-05-31 09:43:05 +0800

    util

--- utils/index.js
+++ utils/index.js
@@ -29,6 +29,28 @@ export function getFirstUpLetter (l1) {
   return convertPinyin(l1).substr(0, 1)
 }
 
+// 汉字转拼音首字母
+
+export function convertPinyinFirst (l1) {
+  let l2 = l1.length
+  let I1 = ''
+  let reg = new RegExp('[a-zA-Z0-9\\- ]')
+  for (let i = 0; i < l2; i++) {
+    let val = l1.substr(i, 1)
+    let name = arraySearch(val, PinYin, true)
+    if (reg.test(val)) {
+      I1 += val.toUpperCase()
+    } else if (name !== false) {
+      I1 += name
+    }
+  }
+  I1 = I1.replace(/ /g, '-')
+  while (I1.indexOf('--') > 0) {
+    I1 = I1.replace('--', '-')
+  }
+  return I1
+}
+
 // 汉字转拼音
 export function convertPinyin (l1) {
   let l2 = l1.length
@@ -50,10 +72,14 @@ export function convertPinyin (l1) {
   return I1
 }
 // 在对象中搜索
-function arraySearch (l1, l2) {
+function arraySearch (l1, l2, flage) {
   for (let name in PinYin) {
     if (PinYin[name].indexOf(l1) !== -1) {
-      return ucfirst(name)
+      if (flage) {
+        return getUcfirst(name)
+      } else {
+        return ucfirst(name)
+      }
     }
   }
   return false
@@ -64,6 +90,14 @@ function ucfirst (l1) {
     let first = l1.substr(0, 1).toUpperCase()
     let spare = l1.substr(1, l1.length)
     return first + spare
+  }
+}
+
+//  获得大写首字母
+function getUcfirst (l1) {
+  if (l1.length > 0) {
+    let first = l1.substr(0, 1).toUpperCase()
+    return first
   }
 }
 
