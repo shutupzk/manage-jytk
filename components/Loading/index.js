62e2f81f2bc58ffe9850e0e2e12522f0a2acc078
吕银蕾 <lvlei14@163.com>
2017-07-14 18:37:37 +0800

    同上

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import theme from '../theme.js';
+import {theme} from 'components';
 
 function Loading (props) {
   return (
@@ -10,24 +10,6 @@ function Loading (props) {
             <div className='loading'>
               <i className='fa fa-spinner fa-spin'></i>
             </div>
-            <style jsx>{`
-              .loading {
-                position: absolute;
-                width: 90px;
-                height: 80px;
-                top: 200px;
-                left: 50%;
-                font-size: 27px;
-                color: #337ab7;
-                text-align: center;
-                line-height: 77px;
-                margin: 0 auto;
-                margin-left: -45px;
-                background: ${theme.maincolor};
-                border-radius: 7px;
-                z-index: 100;
-              }
-            `}</style>
           </div> : ''
       }
     </div>
